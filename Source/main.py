983302eaaa7c39a61e01ea12351ebc93d84c0bf7
Valentin Weyer <valentin.weyer@gmail.com>
2025-09-01 19:41:02 +0200

    Fixed Readme

--- Source/main.py
+++ Source/main.py
@@ -5,7 +5,6 @@ import seed_data
 @cli.command("seed-db")
 def seed_db():
     """📊 Seeds the database with some sample data."""
-    database.create_db_and_tables()
     seed_data.seed_data()
 
 if __name__ == "__main__":
